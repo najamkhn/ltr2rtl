03fa3bfa7ef26ea7f68db6c3fd69683aec28eb54
Najam Khan <najamkhn@gmail.com>
2014-11-13 18:31:08 +0400

    master: adding js file

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ var express = require("express");
 var harp = require("harp");
 var app = express();
 
-// app.configure(function(){
-   app.use(express.static(__dirname + "/src"));
-   app.use(harp.mount(__dirname + "/src"));
-// });
-
+app.use(express.static(__dirname + "/src"));
+app.use(harp.mount(__dirname + "/src"));
 app.listen(9000);
